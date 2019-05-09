b40d70400cf91dc5ab25b7de332d362cd931f450
Inês Justo <inesjusto@ua.pt>
2019-05-09 22:34:44 +0100

    FirstVersion

---
+++ CondominiumManager/CondominiumManager/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CondominiumManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.condoSelectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eventsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maintenanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paymentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.readingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.complaintsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.requestsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.condoSelectToolStripMenuItem,
+            this.eventsToolStripMenuItem,
+            this.maintenanceToolStripMenuItem,
+            this.paymentsToolStripMenuItem,
+            this.readingsToolStripMenuItem,
+            this.contactsToolStripMenuItem,
+            this.complaintsToolStripMenuItem,
+            this.requestsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // condoSelectToolStripMenuItem
+            // 
+            this.condoSelectToolStripMenuItem.Name = "condoSelectToolStripMenuItem";
+            this.condoSelectToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.condoSelectToolStripMenuItem.Text = "CondoSelect";
+            // 
+            // eventsToolStripMenuItem
+            // 
+            this.eventsToolStripMenuItem.Name = "eventsToolStripMenuItem";
+            this.eventsToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.eventsToolStripMenuItem.Text = "Events";
+            this.eventsToolStripMenuItem.Click += new System.EventHandler(this.Events_Click);
+            // 
+            // maintenanceToolStripMenuItem
+            // 
+            this.maintenanceToolStripMenuItem.Name = "maintenanceToolStripMenuItem";
+            this.maintenanceToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.maintenanceToolStripMenuItem.Text = "Maintenance";
+            this.maintenanceToolStripMenuItem.Click += new System.EventHandler(this.Maintenance_Click);
+            // 
+            // paymentsToolStripMenuItem
+            // 
+            this.paymentsToolStripMenuItem.Name = "paymentsToolStripMenuItem";
+            this.paymentsToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.paymentsToolStripMenuItem.Text = "Payments";
+            this.paymentsToolStripMenuItem.Click += new System.EventHandler(this.Payments_Click);
+            // 
+            // readingsToolStripMenuItem
+            // 
+            this.readingsToolStripMenuItem.Name = "readingsToolStripMenuItem";
+            this.readingsToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.readingsToolStripMenuItem.Text = "Readings";
+            this.readingsToolStripMenuItem.Click += new System.EventHandler(this.Readings_Click);
+            // 
+            // contactsToolStripMenuItem
+            // 
+            this.contactsToolStripMenuItem.Name = "contactsToolStripMenuItem";
+            this.contactsToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.contactsToolStripMenuItem.Text = "Contacts";
+            this.contactsToolStripMenuItem.Click += new System.EventHandler(this.Contacts_Click);
+            // 
+            // complaintsToolStripMenuItem
+            // 
+            this.complaintsToolStripMenuItem.Name = "complaintsToolStripMenuItem";
+            this.complaintsToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.complaintsToolStripMenuItem.Text = "Complaints";
+            this.complaintsToolStripMenuItem.Click += new System.EventHandler(this.Complaints_Click);
+            // 
+            // requestsToolStripMenuItem
+            // 
+            this.requestsToolStripMenuItem.Name = "requestsToolStripMenuItem";
+            this.requestsToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.requestsToolStripMenuItem.Text = "Requests";
+            this.requestsToolStripMenuItem.Click += new System.EventHandler(this.Requests_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "CondominiumManager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem condoSelectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eventsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem maintenanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paymentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem readingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem requestsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem complaintsToolStripMenuItem;
+    }
+}
+
---
+++ CondominiumManager/CondominiumManager/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CondominiumManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Events_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Maintenance_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Payments_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Readings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Contacts_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Requests_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Complaints_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
